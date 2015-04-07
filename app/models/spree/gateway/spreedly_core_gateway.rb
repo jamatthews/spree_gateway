0b21bd07fb27e6aedef8f355745117684633b36c
Jacob Matthews <jake@hell.co.nz>
2015-04-08 01:16:35 +1200

    stored payment profile creation and make capturing submission event conditional on spreedly being selected payment method

--- app/models/spree/gateway/spreedly_core_gateway.rb
+++ app/models/spree/gateway/spreedly_core_gateway.rb
@@ -4,6 +4,15 @@ module Spree
     preference :password, :string
     preference :gateway_token, :string
 
+
+    CARD_TYPE_MAPPING = {
+      'American Express' => 'american_express',
+      'Diners Club' => 'diners_club',
+      'Visa' => 'visa',
+      'MasterCard' => 'master'
+    }
+
+
     def provider_class
       ActiveMerchant::Billing::SpreedlyCoreGateway
     end
@@ -11,13 +20,17 @@ module Spree
     def method_type
       'spreedly_core'
     end
+    
+    def payment_profiles_supported?
+      true
+    end
 
     def purchase(money, creditcard, gateway_options)
       provider.purchase(*options_for_purchase_or_auth(money, creditcard, gateway_options))
     end
 
     def authorize(money, creditcard, gateway_options)
-      provider.authorize(*options_for_purchase_or_auth(money, creditcard, gateway_options))
+        provider.authorize(*options_for_purchase_or_auth(money, creditcard, gateway_options))
     end
 
     def capture(money, response_code, gateway_options)
@@ -32,12 +45,22 @@ module Spree
       provider.void(response_code, {})
     end
 
+    def create_profile(payment)
+      if payment.source.gateway_customer_profile_id.nil?
+        payment.source.update_attributes({
+          :gateway_customer_profile_id => :gateway_payment_profile_id
+        })
+      end
+    end
+
     private
 
     def options_for_purchase_or_auth(money, creditcard, gateway_options)
       options = {}
       options[:description] = "Spree Order ID: #{gateway_options[:order_id]}"
       options[:currency] = gateway_options[:currency]
+      options[:store] = true
+      
 
       if customer = creditcard.gateway_customer_profile_id
         options[:customer] = customer
@@ -50,6 +73,5 @@ module Spree
       end
       return money, creditcard, options
     end
-
-  end
+  end 
 end
